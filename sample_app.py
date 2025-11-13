9e73548d7b85a41770593977164f62018f7fa03c
NabilFahmi16 <nabilfahmi1603@gmail.com>
2025-11-14 07:49:29 +0800

    Fix: Flask on port 5050

--- sample_app.py
+++ sample_app.py
@@ -1,13 +1,12 @@
-# Add to this file for the sample app lab
-from flask import Flask
-from flask import request
-from flask import render_template
+#!/usr/bin/env python3
+from flask import Flask, render_template
 
-sample = Flask(__name__)
+app = Flask(__name__)
 
-@sample.route("/")
+@app.route("/")
 def main():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=8080)
+    # Listen on ALL interfaces, port 5050
+    app.run(host="0.0.0.0", port=5050)
